bdf2790b6906e4ce28d0146e250b4573e8df7d70
Sheva Das <shevadas@gmail.com>
2014-09-20 14:39:47 -0400

    Setting up app

--- myproject/matcher/models.py
+++ myproject/matcher/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Hacker(models.Model):
+    DESIGNER = 'DS'
+    DEVELOPER = 'DV'
+    OTHER = 'OT'
+    SKILL_CHOICES = (
+        (DESIGNER, 'Designer'),
+        (DEVELOPER, 'Developer'),
+        (OTHER, 'Other'),
+    )
+    MALE = 'M'
+    FEMALE = 'F'
+    OTH = 'O'
+    GENDERS = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTH, 'Other')
+    )
+    full_name = models.CharField(max_length=200)
+    gender = models.CharField(max_length=2,
+                             choices=GENDERS)
+    email = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    details = models.TextField()
+
+    skill = models.CharField(max_length=2,
+                             choices=SKILL_CHOICES,
+                             default=DESIGNER)
+    looking_for = models.CharField(max_length=2,
+                             choices=SKILL_CHOICES,
+                             default=DEVELOPER)
